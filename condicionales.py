76f5516d6979189fd91abb188c4f5ade10a30fbb
jhon-daza <93998869+jhon-daza@users.noreply.github.com>
2021-11-30 00:06:53 -0500

    Create condicionales.py

---
+++ condicionales.py
@@ -0,0 +1,15 @@
+##CONDICIONES
+
+number = 15
+
+print("por favor ingrese un numero ")
+
+userNumber = int(input())
+
+if userNumber==number:
+    print("el numero es igual a ", number)
+
+elif userNumber<number:
+    print("el numero es menor a ", number)
+else:
+    print("el numero es mayor a ", number)
